743c87ab3a1dcc2c492e69ca94b3ffdc39b05454
manuelbetancourt <manu.fb55@gmail.com>
2023-03-01 12:48:52 -0500

    creacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WorksComponent } from './works/works.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorksComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,38 +1,10 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements  OnInit, AfterViewInit  {
-  @ViewChild('asTitle')
-  asTitle!: ElementRef;
-  
-  ngOnInit(): void {
-  
-  }
-  AfterViewInit():void {
-    this.initEffect();
-  }
-
-  initEffect = () => {
-    const target= this.asTitle.nativeElement;
-    const writer = new Typewriter(target, {
-      loop: true,
-      typeColor: 'blue'
-    })
-    
-    writer
-      .type('A simple syntax makes it easy.')
-      .rest(500)
-      .start()
-              
-  }   
+export class SidebarComponent {
 
 }
-
- 
-
-
