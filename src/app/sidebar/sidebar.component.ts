7b9dc5c63f2663a03060675c5dc226c68d678a8a
manuelbetancourt <manu.fb55@gmail.com>
2023-03-02 14:33:09 -0500

    t-write

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,9 +11,19 @@ export class SidebarComponent implements OnInit, AfterViewInit {
     throw new Error('Method not implemented.');
   }
 
-
+constructor(){};
   ngOnInit(): void {
+    const target = document.querySelector('.tw');
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeColor: 'var(--color-1)'
+    })
     
+    writer
+      .type('Soy un programador junior con experiencia en lenguajes como JavaScript, Java y Python, así como en tecnologías como Angular, Vue, NodeJs, Bootstrap y WordPress. Tengo estudios acreditados por la Universidad San Tomas y la Universidad Nacional de Colombia, y estoy comprometido en continuar desarrollando mis habilidades y conocimientos en este campo en constante evolución.')
+      .rest(5000)
+      .start()
+
   }
 
 }
