52d8fe4ff1e147a05870cb9cec0309e0da5143ae
manuelbetancourt <manu.fb55@gmail.com>
2023-03-03 16:57:50 -0500

    linkspages

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,15 +7,29 @@ import * as Typewriter from 't-writer.js';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit, AfterViewInit {
-  ngAfterViewInit(): void {
+  
+  links: Array<linksModel>=[
+    {
+      link:'https://github.com/mfbetancourtp',
+      icon: '<i class="uil uil-github"></i>'
+    },
+    {
+      link:'https://www.linkedin.com/in/manuel-fernando-betancourt-paez-515735194/',
+      icon: '<i class="uil uil-linkedin"></i>'
+    }, 
+    {
+      link:'mailto:manu.fb55@gmail.com',
+      icon: '<i class="uil uil-envelope"></i>'
+    }
     
-
+    
+  ];
+  ngAfterViewInit(): void {
     const target = document.querySelector('.tw');
     const writer = new Typewriter(target, {
       loop: true,
       typeColor: 'var(--color-1)'
     })
-    
     writer
       .type('Manuel Betancourt P')
       .rest(5000)
@@ -27,3 +41,9 @@ export class SidebarComponent implements OnInit, AfterViewInit {
   
 
 }
+
+class linksModel{
+  link!: String;
+  icon!: String;
+
+};
