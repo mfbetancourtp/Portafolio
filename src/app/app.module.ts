198b6e3f84bacba5cf5f60a0163235f3a240aaf5
manuelbetancourt <manu.fb55@gmail.com>
2023-03-07 16:28:32 -0500

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { WorksComponent } from './works/works.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorksComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    AboutMeComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule
